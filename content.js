2c0c563405fc55eaa5f5b5d4cc9ee8bb21499d01
Alin-Barsan <alinbarsan@outlook.com>
2021-06-17 22:16:46 -0700

    Fix spacing

--- content.js
+++ content.js
@@ -1,16 +1,16 @@
 /*
-	This file contains a script that tries to:
-	- Identify the section containing the total portfolio value
-	- Extract the value from that section and save it
-	- Identify all sections containing the values of stock plan accounts
-	- Extract the value from those sections and subtract it from the saved total value
-	- Write the resulting value (original total - stock plan accounts) back to the section containing the portfolio value
+    This file contains a script that tries to:
+    - Identify the section containing the total portfolio value
+    - Extract the value from that section and save it
+    - Identify all sections containing the values of stock plan accounts
+    - Extract the value from those sections and subtract it from the saved total value
+    - Write the resulting value (original total - stock plan accounts) back to the section containing the portfolio value
 */
 
 // Define a function that turns a string like "$1,000.23" to a numeric value we can operate on.
 // Taken from: https://www.geeksforgeeks.org/how-to-convert-a-currency-string-to-a-double-value-with-jquery-or-javascript/
 function convert(num) {
-	// Using replace() method to make currency string suitable for parseFloat() to convert 
+    // Using replace() method to make currency string suitable for parseFloat() to convert 
     return parseFloat(num.replace(/[^0-9.-]+/g,""));
 }
 
@@ -41,13 +41,13 @@ var modifiedNumericTotal = convert(portfolioTotalElement.textContent);
 // First, gets all containers marked as belonging to a stock plan
 var stockPlanContainerElements = document.querySelectorAll("[data-acct-type='Stock Plans']");
 stockPlanContainerElements.forEach(element => {
-	// Second, for each stock plan section, find all stock plan account balances
-	var stockPlanBalanceElements = element.getElementsByClassName("account-selector--tab-row account-selector--account-balance  js-acct-balance");
-	Array.from(stockPlanBalanceElements).forEach(balanceElement => {
-		// Third, for each stock plan balance element:
-		// Extract the value, convert it to numeric, and subtract it from the running portfolio total.
-		modifiedNumericTotal = modifiedNumericTotal - convert(balanceElement.innerText);
-	});
+    // Second, for each stock plan section, find all stock plan account balances
+    var stockPlanBalanceElements = element.getElementsByClassName("account-selector--tab-row account-selector--account-balance  js-acct-balance");
+    Array.from(stockPlanBalanceElements).forEach(balanceElement => {
+        // Third, for each stock plan balance element:
+        // Extract the value, convert it to numeric, and subtract it from the running portfolio total.
+        modifiedNumericTotal = modifiedNumericTotal - convert(balanceElement.innerText);
+    });
 });
 
 // Write the resulting value (original total - stock plan accounts) back to the section containing the portfolio value
