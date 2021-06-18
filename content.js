4e30aea69e8fdb58fb7e88dd802b4251905d9f16
Alin-Barsan <alinbarsan@outlook.com>
2021-06-17 22:53:23 -0700

    Add observer on changes to the portfolio total
    https://github.com/Alin-Barsan/Fidelity-Portfolio-Total-Chrome-Extension/issues/1
    
    When the total was updated by navigating around (or just by it legitimately changing from portfolio value changing), our script didn't do anything because it was written to statically execute once.  
    
    Added an observer to listen to changes to the total, and then recalculate and update it given those changes.  
    Was careful to disable the observer when making those changes as we could trigger our own observer and crash the page with an infinite loop.

--- content.js
+++ content.js
@@ -22,33 +22,61 @@ var formatter = new Intl.NumberFormat('en-US', {
 });
 
 
-// This first section has a few lazy assumptions that may need updating if Fidelity updates their site:
-// 1. The class name of the element containing the value is hardcoded here.
-// 2. This assumes there's only one element matching this hardcoded class.  It selects and uses the first match.
-// 3. This assumes the value is contained directly in this element (not in some sub-section within it).
-// All of these assumptions held true in Fidelity's web page structure at the time when I made this.  Things may change.
+// Define the pointer to the element that contains the portfolio total outside the function so we can access this elsewhere
+var portfolioTotalElement;
 
-// Identify the section containing the total portfolio value
-var portfolioTotalElement = document.getElementsByClassName('account-selector--tab-row account-selector--all-accounts-balance js-portfolio-balance')[0];
+// Save the process of updating the portfolio total so we can have the extension repeat this logic as needed
+function updatePortfolioTotal() {
 
-// Extract the value from that section and save it
-var modifiedNumericTotal = convert(portfolioTotalElement.textContent);
+    // This first section has a few lazy assumptions that may need updating if Fidelity updates their site:
+    // 1. The class name of the element containing the value is hardcoded here.
+    // 2. This assumes there's only one element matching this hardcoded class.  It selects and uses the first match.
+    // 3. This assumes the value is contained directly in this element (not in some sub-section within it).
+    // All of these assumptions held true in Fidelity's web page structure at the time when I made this.  Things may change.
 
-// Note: similar assumptions as described earlier are made for the Stock Plan accounts.
-// Thus, this logic may also need updates over time if fidelity changes things.
+    // Identify the section containing the total portfolio value
+    portfolioTotalElement = document.getElementsByClassName('account-selector--tab-row account-selector--all-accounts-balance js-portfolio-balance')[0];
 
-// Identify all sections containing the values of stock plan accounts
-// First, gets all containers marked as belonging to a stock plan
-var stockPlanContainerElements = document.querySelectorAll("[data-acct-type='Stock Plans']");
-stockPlanContainerElements.forEach(element => {
-    // Second, for each stock plan section, find all stock plan account balances
-    var stockPlanBalanceElements = element.getElementsByClassName("account-selector--tab-row account-selector--account-balance  js-acct-balance");
-    Array.from(stockPlanBalanceElements).forEach(balanceElement => {
-        // Third, for each stock plan balance element:
-        // Extract the value, convert it to numeric, and subtract it from the running portfolio total.
-        modifiedNumericTotal = modifiedNumericTotal - convert(balanceElement.innerText);
+    // Extract the value from that section and save it
+    var modifiedNumericTotal = convert(portfolioTotalElement.textContent);
+
+    // Note: similar assumptions as described earlier are made for the Stock Plan accounts.
+    // Thus, this logic may also need updates over time if fidelity changes things.
+
+    // Identify all sections containing the values of stock plan accounts
+    // First, gets all containers marked as belonging to a stock plan
+    var stockPlanContainerElements = document.querySelectorAll("[data-acct-type='Stock Plans']");
+    stockPlanContainerElements.forEach(element => {
+        // Second, for each stock plan section, find all stock plan account balances
+        var stockPlanBalanceElements = element.getElementsByClassName("account-selector--tab-row account-selector--account-balance  js-acct-balance");
+        Array.from(stockPlanBalanceElements).forEach(balanceElement => {
+            // Third, for each stock plan balance element:
+            // Extract the value, convert it to numeric, and subtract it from the running portfolio total.
+            modifiedNumericTotal = modifiedNumericTotal - convert(balanceElement.innerText);
+        });
+    });
+
+    // Write the resulting value (original total - stock plan accounts) back to the section containing the portfolio value
+    portfolioTotalElement.innerHTML = formatter.format(modifiedNumericTotal);
+}
+
+// Update the portfolio total once on page load
+updatePortfolioTotal()
+
+// Configure an observer to listen to changes to the portfolio total - this indicates that Fidelity has refreshed / recalculated the total,
+// so we should too.  Note that we can also trigger this... so we updated the portfolio already above this definition.
+// This implementation is from: https://stackoverflow.com/questions/29405279/detect-div-change-in-javascript
+var config = { attributes: true, childList: true, characterData: true }
+var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        // At this point we have detected that the portfolio total has changed.  But we don't want to end up in an infinite loop,
+        // so we disconnect the observer, THEN write our updated portfolio total, then re-register the observer on the element.
+        observer.disconnect();
+        updatePortfolioTotal();
+        observer.observe(portfolioTotalElement, config);
     });
 });
 
-// Write the resulting value (original total - stock plan accounts) back to the section containing the portfolio value
-portfolioTotalElement.innerHTML = formatter.format(modifiedNumericTotal);
+// Register the observer on the portfolio total element - this is the first time it's registered, as the above is just the configuration
+// on how it behaves when the mutation of the watched element is observed.
+observer.observe(portfolioTotalElement, config);
